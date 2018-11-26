28e69fb0c3ac974ee9cf0a16cffd25a4db13ea7b
exstyle <exstyle@hotmail.fr>
2018-11-26 16:57:02 +0100

    dev

--- MyFirstSharpApp/Program.fs
+++ MyFirstSharpApp/Program.fs
@@ -2,7 +2,22 @@
 
 open System
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+//[<EntryPoint>]
+//let main argv =
+//    printfn "Hello World from F#!"
+//    0 // return an integer exit code
+
+
+[<EntryPoint>]                          
+let main argv =  
+    let items = argv.Length
+    printfn "Passed in %d items: %A" argv.Length argv           
+    let s = Console.ReadLine()
+    0 // return an integer exit code    4
+
+    //  %d => int
+    //  %f => float
+    //  %b => Boolean
+    //  %s => string
+    //  %0 => .ToString()
+    //  %A => F# pretty-print representation of the argument. if none exists => %0
---
+++ MyFirstSharpApp/scratchpad.fsx
@@ -0,0 +1,27 @@
+﻿open System.Linq
+open System.IO
+
+
+let text = "Hello, world" 
+//Console.WriteLine(text.Length)
+text = "Hello, world"
+
+
+
+let greetPersone name age =
+    sprintf "Hello, %s. You are %d years old" name age
+    
+
+
+greetPersone "Sébastien" 34
+
+
+let countWords (sentence:string) = 
+    let age = sentence.Split(' ').ToList().Count
+    use sw = new StreamWriter("C:\\age.fsharp",false)
+    sw.WriteLine(sentence)
+    sw.WriteLine(sprintf ": %d" age)
+
+
+
+countWords "Je m'appelles Sébastien, et si tu veux savoir l'age que j'ai, il va faloire écrire beaucoup de chose, n'est-ce pas ? Je ne sais meme pas comment commencer, alors il va faloir improviser !"
